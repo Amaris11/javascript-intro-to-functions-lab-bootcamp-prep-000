7c7755fabc54566f4c6f4dfc29f593f4be7e5ae6
Amaris <monyei96@hotmail.com>
2018-07-03 16:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
   return "I can\'t hear you!"
 }
 function sayHiToGrandma(string) {
-  if ("HELLO")
+  if ("string.toUpperCase")
   return "YES INDEED!"  
   }
 function sayHiToGrandma(string) {
